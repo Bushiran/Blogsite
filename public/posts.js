5f05e2b050d64aa9a12f31a109f7ffc0bef086c6
Bushiran Ibrahim Witol <80077495+Bushiran@users.noreply.github.com>
2022-04-01 12:36:37 +0000

    navigate

--- public/posts.js
+++ public/posts.js
@@ -3,16 +3,17 @@
         "id": 1,
         "image": "1.png",
         "slug": "post-number-one",
-        "title": "So, what is the Cause",
-        "extract": "The quick brown fox jumps over the lazy dog",
-        "body": " So what happens to the lazy dog"
+        "title": "so what is the cause",
+        "extract": "The quick brown fox jump over the lazy dog",
+        "body": "so what happens to the lazy dog"
     },
-    {
-        "id": 1,
-        "image": "1.png",
-        "slug": "post-number-one",
-        "title": "So, what is the Cause",
-        "extract": "The quick brown fox jumps over the lazy dog",
-        "body": " So what happens to the lazy dog"
-    },
-]
+        {
+            "id": 1,
+            "image": "1.png",
+            "slug": "post-number-one",
+            "title": "so what is the cause",
+            "extract": "The quick brown fox jump over the lazy dog",
+            "body": "so what happens to the lazy dog"
+        },
+    ]
+    
