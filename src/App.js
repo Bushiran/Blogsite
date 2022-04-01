1b8a2517225c3e4981621c3aa9d9817be07d6ab0
Bushiran Ibrahim Witol <80077495+Bushiran@users.noreply.github.com>
2022-04-01 12:30:17 +0000

    navigate

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Home from './pages/Home'
 import {Routes, Route} from "react-router-dom"
 
-function App() {
+function App () {
   return (
     <Routes>
-    <Route path="/" element={<Home/>}/>
+    <Route path="/" element={<Home/>} />
     </Routes>
+    
   )
 }
 
